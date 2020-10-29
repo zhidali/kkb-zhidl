9788470180d6492cec76b759526834608658f201
zhidl <zhidali@julive.com>
2020-10-29 15:51:19 +0800

    vue page

---
+++ zhidl-v3/src/pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class='home'>
+      home
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        components: {},
+        data() {
+            return {};
+        },
+        computed: {},
+        watch: {},
+        created() {},
+        mounted() {},
+        methods: {},
+    };
+</script>
+
+<style lang='less' scoped>
+</style>
