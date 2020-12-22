bd348f52f07b7a931221dcf88db5260d28c31d52
zhidl <zhidali@julive.com>
2020-12-22 10:00:43 +0800

    1

---
+++ zhidl-webpack/webpack1/src/index.js
@@ -0,0 +1,8 @@
+/*
+ * @author: zhidl
+ * @Date: 2020-12-21 20:59:42
+ * @description: 
+ * @LastEditTime: 2020-12-21 21:02:17
+ * @LastEditors: zhidl
+ */
+console.log(1);
---
+++ zhidl-webpack/webpack1/webpack.config.js
@@ -0,0 +1,11 @@
+/*
+ * @author: zhidl
+ * @Date: 2020-12-21 21:09:16
+ * @description: 
+ * @LastEditTime: 2020-12-21 21:10:05
+ * @LastEditors: zhidl
+ */
+module.exports = {
+  entry: './src/index.js',
+  output: './dist',
+}
