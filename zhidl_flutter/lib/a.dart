8eb7a42b8c96e3dade8a1f5df057150d4673fdd9
zhidl <zhidali@julive.com>
2021-02-03 09:47:03 +0800

    1

---
+++ zhidl_flutter/lib/a.dart
@@ -0,0 +1,14 @@
+class Ww { 
+  int n;
+  void getN() {
+    print('n: $n');
+  }
+}
+// 暗号：你好dart
+Class Ppt extends Ww {
+
+}
+var ppt = Ppt();
+ppt.n = 111;
+print(ppt.n);
+ppt.getN();
--- zhidl_flutter/lib/main.dart
+++ zhidl_flutter/lib/main.dart
@@ -58,56 +58,51 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+  @override
+  void diapose() {
+    super.dispose();
+    _controller.diapose();
+  }
 
+
+
+
+  int currentIndex = 0;
+  list<Widget> pages = [
+    Center(child:Text('首页')),
+    Center(child:Text('发现')),
+    Center(child:Text('我的'))
+  ]
+  PageController _controller;
+  
+  @override
+  void initState() {
+    super.initState();
+    _controller = PageController(initialPage:0)
+  }
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // 暗号：我要涨薪
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: PageView(
+        Controller :_controller,
+        physics: NeverScroll
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BorromNavigationBar(
+        currentIndex: currentIndex,
+        onTap:(int index) {
+          setState(() {
+            currentIndex = index;
+            _controller.jumpToPage(index);
+          });
+        },
+        item: [
+          BorromNavigationBarItem(icon: Icon(Icon.home), label: '首页'),
+          BorromNavigationBarItem(icon: Icon(Icon.home), label: '发现'),
+          BorromNavigationBarItem(icon: Icon(Icon.home), label: '我的')
+
+        ]
+      )
     );
   }
 }
