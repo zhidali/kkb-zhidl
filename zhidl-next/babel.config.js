e2826fafde8de696fc1d5486e066ef62f0c3022f
zhidl <zhidali@julive.com>
2021-07-05 17:23:02 +0800

    next.js

---
+++ zhidl-next/babel.config.js
@@ -0,0 +1,11 @@
+/*
+ * @author: zhidl
+ * @Date: 2021-07-05 17:04:09
+ * @description: 
+ * @LastEditTime: 2021-07-05 17:04:28
+ * @LastEditors: zhidl
+ */
+module.exports ={
+  "presets": ["next/babel"],
+  "plugins": [["styled-components", { "ssr": true }]]
+}
---
+++ zhidl-next/pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return <div>Hello nextjs-ts-redux-antd-starter</div>
+}
+
+export default Home
